0747bf2c137680fe94ebb378ca49d06721155297
thanasis xan <36565555+thanasisxan@users.noreply.github.com>
2020-03-25 18:21:23 +0200

    reformat

--- s-lorawan-sim.py
+++ s-lorawan-sim.py
@@ -1,40 +1,40 @@
 import simpy
 
-
 print(simpy.__version__)
 
+channelBusy = False
 
-channelBusy=False
 
 class Packet:
     def __init__(self):
-        self.ACK=False
-        self.COL=False
+        self.ACK = False
+        self.COL = False
+
 
 class Gateway:
     def __init__(self):
         super().__init__()
-        
-        
+
     def receivePacket(self, packet):
         global channelBusy
-        
-        if(channelBusy):
-            packet.COL=True
+
+        if (channelBusy):
+            packet.COL = True
         else:
-            channelBusy=True
+            channelBusy = True
             # wait rx1 delay+timeslot
-            packet.ACK=True #packet is aknowledged
-        
+            packet.ACK = True  # packet is aknowledged
+
         return packet
-    
+
+
 class EndNode:
     def __init__(self):
         super().__init__()
-        
+
     def sendPacket(self, gateway, packet):
         global channelBusy
+        # wait send at next timeslot
         packet = gateway.receivePacket(packet)
-        if(packet.ACK):
-            #wait 1 timeslot
-            channelBusy=False
+        if (packet.ACK):
+            channelBusy = False
